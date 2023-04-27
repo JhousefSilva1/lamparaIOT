6cf24f3dc4cb7874be9187760f4c94b7e763a2ca
Jose Pozo Silva <jhosede23@gmail.com>
2023-04-27 01:54:42 -0400

    first commit

---
+++ lampara_iot-main/config.h
@@ -0,0 +1,3 @@
+const char* ssid = "XXX_nameOfNetwork_XXX";         
+const char* password =  "XXX_password_XXX";
+
---
+++ lampara_iot-main/lampara/config.h
@@ -0,0 +1,2 @@
+const char* ssid = "Flia Silva";         
+const char* password =  "AZ2078Derguplot8541";
